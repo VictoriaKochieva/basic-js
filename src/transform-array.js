24b517f4bd97b3e76f7ce6729a2f8975739e49c6
VictoriaKochieva <kochievavictoria@gmail.com>
2021-09-06 20:35:34 +0300

    feat: add solution in task transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,52 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+let controlSequences = ['--discard-next', '--discard-prev', '--double-next', '--double-prev']
+
+export default function transform(arr) {
+  if (!Array.isArray(arr) || arr === { 'foo': 'bar' }) {
+    throw new Error ("'arr' parameter must be an instance of the Array!")
+  }
+
+  let copiedArr = [...arr]
+
+  for (let i = 0; i < copiedArr.length; i++){
+
+    if (copiedArr[i] === controlSequences[3]) {
+      copiedArr[i] = copiedArr[i-1]
+      if (!copiedArr[i-1]) {
+        copiedArr[i] = ""
+      }
+    }   
+    
+    if (copiedArr[i] === controlSequences[2]) {
+      copiedArr.splice(copiedArr[i-1], 1, copiedArr[i+1])
+      if (!copiedArr[i+1]) {
+        copiedArr[i] = ""
+      }
+    }  
+    
+     if (copiedArr[i] === controlSequences[1]) {
+       copiedArr[i] = ""
+       copiedArr[i-1] = ""       
+    }
+    
+    if (copiedArr[i] === controlSequences[0]){
+      if (!copiedArr[i+1]) {
+        copiedArr[i] = ""
+      }
+
+      copiedArr[i] = ""
+      copiedArr[i+1] = ""
+      
+      if ( copiedArr[i+2] === controlSequences[1] ) {
+      copiedArr[i] = ""
+      copiedArr[i+1] = ""
+      copiedArr[i+2] = ""
+      }
+    } 
+  }
+ let result =  copiedArr.filter(el => el !== "")
+  return result  
 }
