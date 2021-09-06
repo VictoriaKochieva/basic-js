3c07b04f71b37be5bd9ed7747410434671697183
VictoriaKochieva <kochievavictoria@gmail.com>
2021-09-06 22:02:13 +0300

    feat: add part of solution in what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -16,27 +16,43 @@ export default function getSeason(date) {
     return 'Unable to determine the time of year!'
   }
 
+  
+  if (Object.prototype.toString.call(date) === '[object Date]') {  
+
+    if (isNaN (date.getTime ())) {
+      throw new Error ('Invalid date!')
+    } else {
+
   let month = date.getMonth()
 
   switch(month) {
     case 0:
     case 1:
-    case 11: return 'winter'
-    break;
+    case 11: 
+     return 'winter'
 
     case 2:
     case 3:
-    case 4: return 'spring'
-    break;
+    case 4: 
+     return 'spring'
+ 
 
     case 5:
     case 6:
-    case 7: return 'summer'
-    break;
+    case 7: 
+     return 'summer'
+  
 
     case 8:
     case 9:
-    case 10: return 'autumn'
-    break;
+    case 10: 
+     return 'autumn'
+   
   }
+}
+
+}
+else {
+  throw new Error ('Invalid date!')
+}
 }
